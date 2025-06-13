324a040a16a98a72858d97619f59a8a164749f94
AdrianBellazco <adrian.saavedra01@uceva.edu.co>
2025-06-13 17:50:14 -0500

    creacion de la primera parte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
---
+++ src/app/components/nambar/nambar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nambar',
+  standalone: true,
+  imports: [],
+  templateUrl: './nambar.component.html',
+  styleUrl: './nambar.component.css'
+})
+export class NambarComponent {
+
+}
