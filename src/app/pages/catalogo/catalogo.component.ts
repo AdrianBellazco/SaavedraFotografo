1dc3e35c8a8cbc908dfbe172069f727d1f0f1c6e
AdrianBellazco <adrian.saavedra01@uceva.edu.co>
2025-11-04 22:11:53 -0500

    se añadieron los catalogos

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -9,33 +9,66 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./catalogo.component.css']
 })
 export class CatalogoComponent {
-  servicios = [
+  boda = [
     {
-      titulo: 'Fotografía de Bodas',
+      titulo: 'Golden Boda',
       descripcion: 'Capturamos los momentos más importantes de tu día con estilo natural y elegante.',
       detalle: 'Incluye cobertura completa del evento, sesión pre-boda y edición profesional de todas las fotos.',
-      imagen: '/assets/fotos-boda.jpg'
+      imagen: '/assets/p.jpg'
     },
     {
-      titulo: 'Fotos de 15 Años',
+      titulo: 'Platinum Boda',
       descripcion: 'Inmortalizamos este momento único con sesiones creativas y memorables.',
       detalle: 'Sesión personalizada en exteriores o estudio con vestuario, maquillaje y edición de alta calidad.',
       imagen: '/assets/15.jpg'
     },
     {
-      titulo: 'Fotos Urbanas',
+      titulo: 'Escencial Boda',
       descripcion: 'Retratos urbanos con actitud, perfectos para redes sociales y portafolios.',
       detalle: 'Sesiones en locaciones modernas con iluminación natural o artificial según tu estilo.',
       imagen: '/assets/urban.jpg'
     },
     {
-      titulo: 'Fotos para tu Empresa',
+      titulo: 'Digital Boda',
       descripcion: 'Fotografía corporativa profesional para mejorar tu presencia de marca.',
       detalle: 'Incluye retratos de equipo, instalaciones y productos con enfoque empresarial.',
       imagen: '/assets/empresa.jpg'
     },
     {
-      titulo: 'Video Marketing',
+      titulo: 'Escencial Book boda',
+      descripcion: 'Promociona tu marca con videos dinámicos y contenido visual atractivo.',
+      detalle: 'Producción completa de videos publicitarios y reels con enfoque en redes sociales.',
+      imagen: '/assets/video-marketing.jpg'
+    }
+  ];
+
+   quinces = [
+    {
+      titulo: 'Golden quinces',
+      descripcion: 'Capturamos los momentos más importantes de tu día con estilo natural y elegante.',
+      detalle: 'Incluye cobertura completa del evento, sesión pre-boda y edición profesional de todas las fotos.',
+      imagen: '/assets/p.jpg'
+    },
+    {
+      titulo: 'Platinum quinces',
+      descripcion: 'Inmortalizamos este momento único con sesiones creativas y memorables.',
+      detalle: 'Sesión personalizada en exteriores o estudio con vestuario, maquillaje y edición de alta calidad.',
+      imagen: '/assets/15.jpg'
+    },
+    {
+      titulo: 'Escencial quinces',
+      descripcion: 'Retratos urbanos con actitud, perfectos para redes sociales y portafolios.',
+      detalle: 'Sesiones en locaciones modernas con iluminación natural o artificial según tu estilo.',
+      imagen: '/assets/urban.jpg'
+    },
+    {
+      titulo: 'Digital quinces',
+      descripcion: 'Fotografía corporativa profesional para mejorar tu presencia de marca.',
+      detalle: 'Incluye retratos de equipo, instalaciones y productos con enfoque empresarial.',
+      imagen: '/assets/empresa.jpg'
+    },
+    {
+      titulo: 'Escencial Book quinces',
       descripcion: 'Promociona tu marca con videos dinámicos y contenido visual atractivo.',
       detalle: 'Producción completa de videos publicitarios y reels con enfoque en redes sociales.',
       imagen: '/assets/video-marketing.jpg'
